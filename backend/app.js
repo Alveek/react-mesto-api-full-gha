5b83f008c447773ff6626957e8edff359b2bf8a7
Alexey Kudryavtsev <alveek@yandex.ru>
2023-04-30 02:07:23 +0300

    update mongodb localhost

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
